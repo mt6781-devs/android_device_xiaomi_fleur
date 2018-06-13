322ba15c10ae057181fcfb78dc54284af9f36757
Bjoern Johansson <bjoernj@google.com>
2018-06-13 10:57:09 -0700

    fleur: Add support for Wifi-Direct
    CTS and VTS now require support for Wifi-Direct (also known as p2p
    wifi). Add the required xml file to indicate support, fix the
    wpa_supplicant configuration to deal with p2p correectly, and make sure
    the proper directories are created for the p2p configuration.
    
    BUG: 79749857
    Test: run vts -m VtsHalWifiSupplicantV1_0Target
    Change-Id: I0e658a6020c1fc4a7cf7547cbfc6e2b0151f2a03

--- device.mk
+++ device.mk
@@ -191,6 +191,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/wifi/,$(TARGET_COPY_OUT_VENDOR)/etc/wifi) \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 

f3754779521f22a6badcdd0fb12c0183afcf32f1
Bruno Martins <bgcngm@gmail.com>
2019-07-04 13:32:43 +0100

    fleur: Set vendor SPL
    Change-Id: I99c617bed05b4583eb603171b60b672e64cb9073

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,6 +116,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/fstab.mt6781
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2024-12-01
+
 # SEPolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 

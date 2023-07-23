a3d0c754256c41914b95468bdd48e6a35dbc7555
SamarV-121 <samarvispute121@pm.me>
2023-07-23 22:28:30 +0100

    fleur: Use libutils-v32 for libmtkcam_stdutils.so
    * Abort message: 'incStrongRequireStrong() called on 0xb400007c5afa7250 which isn't already owned'
    
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>
    Change-Id: I8e4466f68660ea5dca23a51e624e46561cd73d10

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,9 @@ function blob_fixup {
         vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
+        vendor/lib64/libmtkcam_stdutils.so)
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
+            ;;
     esac
 }
 

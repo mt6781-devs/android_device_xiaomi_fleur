0ca954373c42f9f17322626037fb2e2abdbf3116
Arian <arian.kulmer@web.de>
2023-08-19 16:22:59 +0200

    fleur: Set stock build fingerprint and description
    Change-Id: I9af04943b1b869a57c82f9f2dac6ddf9391162c2

--- lineage_fleur.mk
+++ lineage_fleur.mk
@@ -19,4 +19,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 2201117SY
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="fleur_global-user 13 SP1A.210812.016 V816.0.9.0.TKEMIXM release-keys" \
+    BuildFingerprint=Redmi/fleur_global/fleur:13/SP1A.210812.016/V816.0.9.0.TKEMIXM:user/release-keys
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

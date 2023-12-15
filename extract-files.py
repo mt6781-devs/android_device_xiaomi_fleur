b6af4c870859d4721b3826b00c7ae06a497658ac
bengris32 <bengris32@protonmail.ch>
2023-12-15 22:05:40 +0000

    fleur: Export receivers that aren't exclusively registered for system broadcasts
    Change-Id: Ib39e51e351192f00b92a8887840c73edb19230ff
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -42,6 +42,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system/priv-app/ImsService/ImsService.apk': blob_fixup()
+        .apktool_patch('blob-patches/ImsService/'),
     'system/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
     'system/lib64/libsink-mtk.so': blob_fixup()

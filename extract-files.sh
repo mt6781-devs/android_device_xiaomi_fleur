7ec35f9e69e5e1fc3a31544495d2347ecab97e25
Woomymy <woomy@woomy.be>
2023-12-19 16:09:53 +0000

    fleur: Link media.c2 service against v33 libstagefright_foundation
    * Fixes crashes and high CPU usage
    
    Change-Id: I60c07d10c2db9335d20c6dbaaaeacc9313aebf06
    Signed-off-by: Woomymy <woomy@woomy.be>

--- extract-files.sh
+++ extract-files.sh
@@ -56,6 +56,7 @@ fi
 function blob_fixup {
     case "$1" in
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
+            "$PATCHELF" --add-needed "libstagefright_foundation-v33.so" "$2"
             "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
             ;;
         vendor/bin/hw/android.hardware.gnss-service.mediatek|\

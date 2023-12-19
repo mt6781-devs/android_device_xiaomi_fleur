a0cc3123f4cc5e09dab85b3cb5e737adab721431
Woomymy <woomy@woomy.be>
2023-12-19 16:09:53 +0000

    fleur: Link media.c2 service against v33 libstagefright_foundation
    * Fixes crashes and high CPU usage
    
    Change-Id: I60c07d10c2db9335d20c6dbaaaeacc9313aebf06
    Signed-off-by: Woomymy <woomy@woomy.be>

--- extract-files.py
+++ extract-files.py
@@ -25,6 +25,7 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/init/android.hardware.media.c2@1.2-mediatek-64b.rc': blob_fixup()
         .regex_replace('mediatek', 'mediatek-64b'),
     'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
     ('vendor/bin/mnld', 'vendor/lib64/libaalservice.so', 'vendor/lib64/libcam.utils.sensorprovider.so'): blob_fixup()
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),

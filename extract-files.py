d7bb9e6a51cd88fed8489b70d74dd2913e6ebf43
Bruno Martins <bgcngm@gmail.com>
2024-01-16 22:09:40 +0000

    fleur: Add firmware extraction support
    Rename from factory image dump to the common partition naming.
    
    Change-Id: I61b0f4cdddf65166cb12a39926e1fcb504241975

--- extract-files.py
+++ extract-files.py
@@ -90,6 +90,7 @@ module = ExtractUtilsModule(
     'xiaomi',
     blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':

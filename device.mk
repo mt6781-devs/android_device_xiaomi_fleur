740dd7bac0240480cf298aad556a4b7306241f06
Jooyung Han <jooyung@google.com>
2024-03-22 13:38:49 +0000

    fleur: Install bootcontrol HAL APEX
    Bug: 297467553
    Test: VtsHalBootAidlTargetTest
    Change-Id: Id206966e9f181283082b85dadbf409e3bf4017db

--- device.mk
+++ device.mk
@@ -26,6 +26,10 @@ PRODUCT_SOONG_NAMESPACES += \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 PRODUCT_PACKAGES += \
+    com.android.hardware.boot \
+    android.hardware.boot-service.default_recovery
+
+PRODUCT_PACKAGES += \
     otapreopt_script \
     update_engine \
     update_engine_client \

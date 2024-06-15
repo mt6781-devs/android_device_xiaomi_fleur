e32138736d22de1b5afa9b0517a045d20f1efd7d
Lokesh Gidra <lokeshgidra@google.com>
2024-06-15 11:33:25 +0200

    fleur: Use 6GB Dalvik-heap Config
    Bug: 145823510
    Test: MPTS
    Change-Id: I99b04d088330ad465d2fc09986e975c262f2bdad

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/xiaomi/fleur
 
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 

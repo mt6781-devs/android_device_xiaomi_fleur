3934d88535207beb51f7cf64d5cc7042a5edbcb3
ChrisCatto <chriscatto08@gmail.com>
2024-07-08 16:49:27 +0100

    fleur: Link mediatek camera provider against v32 `libutils`
    * Abort message: 'incStrongRequireStrong() called on 0xb40000770d0587a0
      which isn't already owned'
    * Required after U QPR3
    
    Change-Id: Ie6ea7f331207f604cb87804c67f4de9fd9a60d76
    Signed-off-by: ChrisCatto <chriscatto08@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,7 @@ function blob_fixup {
         vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
+        vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so|\
         vendor/lib64/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;

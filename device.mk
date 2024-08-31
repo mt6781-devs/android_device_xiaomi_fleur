2ab42fee9f7701044ce3e6423c5626bceee20eb8
claxten10 <claxten10@gmail.com>
2024-08-31 11:36:01 +0200

    fleur: Configure Bluetooth stack
    * Reference: https://github.com/moto-common/android_device_mediatek_common/blob/master/vendor/bt/proprietary-files.txt
    
    Co-authored-by: electimon <electimon@gmail.com>
    Co-authored-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -81,6 +81,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1.vendor
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \

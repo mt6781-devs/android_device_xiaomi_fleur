b9387c5d49d9322ade333c26f1bbfcd2f43bdce0
claxten10 <claxten10@gmail.com>
2024-08-31 06:41:20 +0200

    fleur: Define Bootloader and Platform flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,5 +21,14 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := fleur
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6781
+BOARD_HAS_MTK_HARDWARE := true
+BOARD_VENDOR := xiaomi
+
 # Inherit the proprietary files
 include vendor/xiaomi/fleur/BoardConfigVendor.mk

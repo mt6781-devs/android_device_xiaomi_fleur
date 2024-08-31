50e52d0c4194eaf70b11431b8171ce6ee487060d
claxten10 <claxten10@gmail.com>
2024-08-31 21:36:15 +0200

    fleur: Configure DRM stack
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>

--- device.mk
+++ device.mk
@@ -101,6 +101,10 @@ PRODUCT_PACKAGES += \
     libdrm.vendor \
     libutilscallstack.vendor
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm-service.clearkey
+
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false

bbfe30af970e41972eae728c47d9cf94e5772418
Bruno Martins <bgcngm@gmail.com>
2024-09-01 17:25:17 +0200

    fleur: overlay: Enable smooth display with 120Hz
    There's no point to keep using custom pref to set minimum refresh rate
    when AOSP smooth display feature behaves in the exact same way in
    our case.

--- device.mk
+++ device.mk
@@ -252,6 +252,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworksResOverlayFleur \
+    SettingsOverlayFleur \
     SystemUIOverlayFleur \
     TetheringOverlayFleur \
     WifiResOverlayFleur

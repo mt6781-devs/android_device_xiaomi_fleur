9d6a0e34ca143bf0adb449540346aec92fdeb637
SzuWei Lin <szuweilin@google.com>
2024-09-06 23:50:30 +0100

    fleur: Support dynamic 64-bit media service
    64-bit GSI requires support both 64_32-bit and 64-bit devices at
    the same time. Add a new variable TARGET_DYNAMIC_64_32_MEDIASERVER
    to put mediaserver_dynamic.rc into GSI. Which starts 64-bit
    mediaserver if ro.zygote is zygote64. Otherwise, it keeps the
    original behavior to start 32-bit meidaserver.
    
    Bug: 236664614
    Test: make gsi_arm64-user; Check system/etc/init
    Test: make gsi_arm-user; Check systen/etc/init
    Test: cts-tradefed; run cts-on-gsi -m CtsViewTestCases
    Change-Id: I74699ec5fafadcbe78a7fef6d10aaed752cd5afa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,9 @@ ZYGOTE_FORCE_64 := true
 # Include 64-bit drmserver to support 64-bit only devices
 TARGET_DYNAMIC_64_32_DRMSERVER := true
 
+# Include 64-bit mediaserver to support 64-bit only devices
+TARGET_DYNAMIC_64_32_MEDIASERVER := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a-dotprod

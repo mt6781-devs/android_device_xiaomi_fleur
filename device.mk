14e9db9d6b045b78ef85d021f149de21390cf304
claxten10 <claxten10@gmail.com>
2024-09-29 22:38:51 +0000

    fleur: Nuke `libstagefright_softomx_plugin`

--- device.mk
+++ device.mk
@@ -226,7 +226,6 @@ PRODUCT_PACKAGES += \
     libcodec2_hidl@1.1.vendor \
     libcodec2_hidl@1.2.vendor \
     libavservices_minijail_vendor \
-    libstagefright_softomx_plugin.vendor \
     libsfplugin_ccodec_utils.vendor \
     libcodec2_soft_common.vendor
 

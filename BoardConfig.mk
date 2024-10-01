393da833fd2609a669002dd701885c1314fa8fef
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-10-01 12:49:43 +0000

    fleur: Define DT2W node for power HAL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,6 +117,9 @@ TARGET_BOARD_PLATFORM := mt6781
 BOARD_HAS_MTK_HARDWARE := true
 BOARD_VENDOR := xiaomi
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop

0ba828f10c8722191945b5864a5d7e1b5c7cf325
1xtAsh <ashraykrsingh321@gmail.com>
2024-10-05 15:53:54 +0000

    fleur: Build libtensorflowlite

--- device.mk
+++ device.mk
@@ -414,10 +414,13 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/mediatek/libmtkperf_client
 
+# Tensor Flow Lite
+PRODUCT_PACKAGES += \
+    libtensorflowlite_jni
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.mediatek
-
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json

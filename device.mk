736f69bdee3f04b39b88b5ac549b2f8046d1b5df
bengris32 <bengris32@protonmail.ch>
2024-10-06 17:12:04 +0000

    fleur: Switch to common USB service

--- device.mk
+++ device.mk
@@ -428,7 +428,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-mediatekv2
+    android.hardware.usb-service.mediatek
 
 # Vibrator
 PRODUCT_PACKAGES += \

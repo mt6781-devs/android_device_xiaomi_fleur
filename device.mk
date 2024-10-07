d2026366deb6a6a9c7acd6ebcc0826c82fadb524
claxten10 <claxten10@gmail.com>
2024-10-07 17:20:59 +0000

    fleur: Build `PowerOffAlarm`

--- device.mk
+++ device.mk
@@ -353,6 +353,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# PowerOff Alarm
+PRODUCT_PACKAGES += \
+     PowerOffAlarm
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt

b3ecbd2fdf47c3ee4bec98bee5166a2d0b69d918
Bruno Martins <bgcngm@gmail.com>
2024-10-09 12:37:26 +0000

    fleur: Blacklist fingerprint HAL key events
    Change-Id: I57db108f5250c120726b77dd32f4666917c9eb7c

--- device.mk
+++ device.mk
@@ -111,7 +111,9 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint-service.xiaomi
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
+    $(DEVICE_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(DEVICE_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
 # GNSS
 PRODUCT_COPY_FILES += \

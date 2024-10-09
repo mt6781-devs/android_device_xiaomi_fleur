e6497072cc46f069435237377b4abf80b83c623b
claxten10 <claxten10@gmail.com>
2024-10-09 13:12:12 +0000

    fleur: Move symlinking to extract utils
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- Android.mk
+++ Android.mk
@@ -7,22 +7,5 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),fleur)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-include $(CLEAR_VARS)
-
-VULKAN_SYMLINKS := \
-    $(TARGET_OUT_VENDOR)/lib \
-    $(TARGET_OUT_VENDOR)/lib64 \
-    $(TARGET_OUT_VENDOR)/lib/hw \
-    $(TARGET_OUT_VENDOR)/lib64/hw
-
-$(VULKAN_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	$(hide) echo "Linking vulkan libs"
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
-	@ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
-	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
-	$(hide) touch $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(VULKAN_SYMLINKS)
+include $(call all-makefiles-under,$(call my-dir))
 endif

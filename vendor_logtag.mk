15d7f090c821d4cb58f8cd42491aaba312fe9dac
claxten10 <claxten10@gmail.com>
2024-10-09 00:30:17 +0000

    fleur: Shut up hwcomposer logspam

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -263,4 +263,5 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.thermal_src=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.CCodec=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.C2Store=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.MDP=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.MDP=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.hwcomposer=$(VENDOR_LOG_LEVEL)

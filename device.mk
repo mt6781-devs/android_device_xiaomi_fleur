bf4db232d968ab2d2d0a19d25fb1cfa55c20e0c7
claxten10 <claxten10@gmail.com>
2024-12-27 23:43:29 +0000

    fleur: Initial Brightness overlays
    * Mostly adapted from `panther`
    
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -165,6 +165,9 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+PRODUCT_PACKAGES += \
+    FrameworkOverlayFleur
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr

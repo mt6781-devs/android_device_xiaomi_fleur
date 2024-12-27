bfff98e747d345d3b6b9510e1382f893caae9e76
claxten10 <claxten10@gmail.com>
2024-12-27 23:17:40 +0000

    fleur: Configure Thermal stack
    * Use Pixel Thermal HAL instead of the MediaTek one
    * Don't import MediaTek's thermal daemons as they are unneeded.
    * Pull mi_thermald for thermal throttling.
    
    Change-Id: I97208620118404421b4b00ec978941f704bc3397
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -124,6 +124,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.connectivity.rc \
     init.mt6781.rc \
+    init.mt6781.thermal.rc \
     init.project.rc \
     init.sensor_1_0.rc \
     init.recovery.mt6781.rc \
@@ -153,6 +154,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.mediatek
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # WiFi
 PRODUCT_PACKAGES += \

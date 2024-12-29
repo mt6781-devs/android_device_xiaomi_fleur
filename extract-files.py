8bf72a05c9ba2dce69da7270f14e26be3b5a1905
nisel <96194200+nisel11@users.noreply.github.com>
2024-12-29 17:10:10 +0200

    fleur: Switch to python extract-utils
    * Fix libshim_sensors patches while at it.
    
    Signed-off-by: nisel <96194200+nisel11@users.noreply.github.com>

---
+++ extract-files.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import extract_utils.tools
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_17_2'
+
+namespace_imports = [
+	'device/xiaomi/fleur',
+	'hardware/mediatek',
+	'hardware/mediatek/libmtkperf_client',
+	'hardware/xiaomi',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'vendor.mediatek.hardware.videotelephony@1.0',
+    ): lib_fixup_vendor_suffix,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libsink.so': blob_fixup()
+        .add_needed('libshim_sink.so'),
+
+    'system_ext/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so')
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
+
+    ('vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so', 'vendor/bin/hw/android.hardware.gnss-service.mediatek'): blob_fixup()
+        .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+
+    ('vendor/bin/mnld', 'vendor/lib64/libaalservice.so', 'vendor/lib64/libcam.utils.sensorprovider.so'): blob_fixup()
+        .add_needed('libshim_sensors.so'),
+
+    'vendor/etc/init/android.hardware.media.c2@1.2-mediatek-64b.rc': blob_fixup()
+        .regex_replace('mediatek', 'mediatek-64b'),
+
+    'vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc': blob_fixup()
+        .regex_replace('start', 'enable'),
+
+    'vendor/etc/camera/camerabooster.json': blob_fixup()
+        .regex_replace('"sea"', '"fleur"'),
+
+    'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+
+    ('vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so', 'vendor/lib64/libmtkcam_stdutils.so'): blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+    'vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so': blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
+    ('vendor/lib64/vendor.mediatek.hardware.power@1.1.so', 'vendor/lib64/vendor.mediatek.hardware.power@2.0.so'): blob_fixup()
+        .add_needed('libshim_power.so'),
+    'vendor/lib64/libmnl.so': blob_fixup()
+        .add_needed('libcutils.so'),
+    (
+        'vendor/lib/libteei_daemon_vfs.so',
+        'vendor/lib64/libteei_daemon_vfs.so',
+        'vendor/lib64/libSQLiteModule_VER_ALL.so',
+        'vendor/lib64/lib3a.flash.so',
+        'vendor/lib64/lib3a.ae.stat.so',
+        'vendor/lib64/lib3a.sensors.color.so',
+        'vendor/lib64/lib3a.sensors.flicker.so',
+        'vendor/lib64/libaaa_ltm.so',
+    ): blob_fixup()
+        .add_needed('liblog.so'),
+}
+
+module = ExtractUtilsModule(
+    'fleur',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()

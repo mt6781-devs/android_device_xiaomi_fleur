9ffdbc604400b4f523014ba2d565e3931bf5e296
claxten10 <claxten10@gmail.com>
2024-12-29 16:46:31 +0000

    fleur: overlay: Configure paddings
    Change-Id: I7f5d90e3253ec6a97fe50fe65f395fcba5c0c555
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -243,7 +243,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     FrameworkOverlayFleur \
-    SettingsOverlayFleur
+    SettingsOverlayFleur \
+    SystemUIOverlayFleur
 
 PRODUCT_PACKAGES += \
     LineageDialerFleur

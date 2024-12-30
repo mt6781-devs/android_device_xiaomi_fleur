8a99d8b0fd96cea1e0acb601e8a3946023f0f232
claxten10 <claxten10@gmail.com>
2024-12-30 20:38:27 +0000

    fleur: Import stock compatibility matrix
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,6 +137,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # VINTF
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Verified Boot
 BOARD_AVB_ENABLE := true

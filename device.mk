29ea0f246d76ff473485792b06a19aa72b138c2c
claxten10 <claxten10@gmail.com>
2024-12-30 22:04:18 +0000

    fleur: Ship latest control groups and task profiles from system/core
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -98,6 +98,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Control groups/Task profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # ConsumerIR
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.example

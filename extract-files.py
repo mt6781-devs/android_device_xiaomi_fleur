1bac93e7c0129531132cd028d9f68df462abed7b
Giovanni Ricca <giovanniricca@proton.me>
2025-02-14 15:42:12 +0100

    fleur: Rename libsink library to not conflict with the rust module
    Change-Id: I16028606d42d63525b5ef3130d86279bf35e26e1

--- extract-files.py
+++ extract-files.py
@@ -42,6 +42,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libimsma.so': blob_fixup()
+        .replace_needed('libsink.so', 'libsink-mtk.so'),
     'vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc': blob_fixup()
         .regex_replace('start', 'enable'),
     'vendor/etc/init/android.hardware.media.c2@1.2-mediatek-64b.rc': blob_fixup()

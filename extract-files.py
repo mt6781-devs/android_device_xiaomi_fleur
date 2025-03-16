3a7d0169cc53deb642cab3ed0a41486fd05d9a4e
claxten10 <claxten10@gmail.com>
2025-03-16 00:03:53 +0000

    fleur: Link blobs that use the old Trim symbol against libbase_shim
    Change-Id: I4856656e15bd4c0bb23e5963d0f1f25dfe0bcb4e
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -54,7 +54,7 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libaaa_ltm.so',
     ): blob_fixup()
         .add_needed('liblog.so'),
-    ('vendor/lib64/libneuralnetworks_sl_driver_mtk_legacy_prebuilt.so', 'vendor/lib64/libneuron_adapter_mgvi.so'): blob_fixup()
+    'vendor/lib64/libneuron_adapter_mgvi.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_createFromHandle')
         .clear_symbol_version('AHardwareBuffer_describe')
@@ -62,6 +62,17 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_lock')
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib64/libneuralnetworks_sl_driver_mtk_legacy_prebuilt.so': blob_fixup()
+        .clear_symbol_version('AHardwareBuffer_allocate')
+        .clear_symbol_version('AHardwareBuffer_createFromHandle')
+        .clear_symbol_version('AHardwareBuffer_describe')
+        .clear_symbol_version('AHardwareBuffer_getNativeHandle')
+        .clear_symbol_version('AHardwareBuffer_lock')
+        .clear_symbol_version('AHardwareBuffer_release')
+        .clear_symbol_version('AHardwareBuffer_unlock')
+        .add_needed('libbase_shim.so'),
+    ('vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so', 'vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron'): blob_fixup()
+        .add_needed('libbase_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(

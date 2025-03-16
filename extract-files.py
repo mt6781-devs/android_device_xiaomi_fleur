6b98d5bcc37d36567535fe3ea0781d00bd6757e4
claxten10 <claxten10@gmail.com>
2025-03-16 00:45:05 +0000

    fleur: Shim hwcomposer.mtk_common with libprocessgroup_shim
    Change-Id: I1c4b880e80077d87ad5b56c8c15a5488f79c5c67
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -37,6 +37,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libutils.so', 'libutils-v32.so'),
     ('vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so', 'vendor/bin/hw/android.hardware.gnss-service.mediatek'): blob_fixup()
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+    'vendor/lib64/hw/hwcomposer.mtk_common.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
     'vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so': blob_fixup()
         .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
 }  # fmt: skip

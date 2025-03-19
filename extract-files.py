b133f71a0e381e1ab9dffdaba472f86c1b92d5f9
Tuan Anh <tuan73176@gmail.com>
2025-03-19 23:56:38 +0700

    fleur: Import Python extract-utils
    As of LineageOS/android_tools_extract-utils@c0902d93f4b10a14f186bb35944aa13ff7918b50.
    
    Change-Id: If6e4ce195c804f2b69906e5dd1fbd263f2e7fa92

---
+++ extract-files.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/fleur',
+    'hardware/mediatek',
+    'hardware/xiaomi',
+]
+
+module = ExtractUtilsModule(
+    'fleur',
+    'xiaomi',
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()

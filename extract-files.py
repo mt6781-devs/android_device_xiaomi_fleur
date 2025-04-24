0149bea94a595cf5762d176501b504f575e9e9c6
Michael Bestas <mkbestas@lineageos.org>
2025-04-24 10:19:33 +0000

    fleur: Shim libnvram/libsysenv with libbase_shim
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d

--- extract-files.py
+++ extract-files.py
@@ -31,6 +31,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
     ('vendor/bin/mnld', 'vendor/lib64/libaalservice.so', 'vendor/lib64/libcam.utils.sensorprovider.so'): blob_fixup()
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
+    ('vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
+        .add_needed('libbase_shim.so'),
     ('vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so', 'vendor/lib64/libmtkcam_stdutils.so', 'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so'): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
     ('vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so', 'vendor/bin/hw/android.hardware.gnss-service.mediatek'): blob_fixup()

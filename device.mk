e51bea3ae52b88970a42e569fd09fcd4567612d8
LuK1337 <priv.luk@gmail.com>
2025-05-19 23:01:52 +0700

    fleur: Build Lineage common lights HAL
    Change-Id: Ia7610b2bd5de23407bbd9d7e39db67ce46174d03

--- device.mk
+++ device.mk
@@ -150,6 +150,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore.km41.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.lineage
+
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)

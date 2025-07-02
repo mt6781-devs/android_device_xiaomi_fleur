0d6800f9ec0cfa1935491dac03856d0d28e9ff54
claxten10 <claxten10@gmail.com>
2025-07-02 06:40:30 +0000

    fleur: Include common MediaTek/Xiaomi FCM
    Change-Id: Iae902c69c61ad014de3bb5319a4d164e4006d052
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,7 +115,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 
 # Verified Boot
 AB_OTA_PARTITIONS += \

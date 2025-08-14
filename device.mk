24811c2f69508be75055aa94c3f8f5ab47a8f599
Mashopy <eliasgheeraert@gmail.com>
2025-08-14 21:38:47 +0200

    fleur: Switch to AIDL bluetooth service
    Change-Id: I40550f0f2033e35ea609401f38f7646cb79abe14

--- device.mk
+++ device.mk
@@ -98,7 +98,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1-mtk-service
+    android.hardware.bluetooth-service.mediatek
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \

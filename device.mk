c272a2cb15bc3015105a432730008bd36e19dddc
LuK1337 <priv.luk@gmail.com>
2025-11-16 10:56:33 +0100

    fleur: Opt out of speaker_layout_channel_mask field
    Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    Change-Id: I0bf7eafbeb19957326de04ecf7b903580315dc59

--- device.mk
+++ device.mk
@@ -73,6 +73,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_OPTIONAL_system=true
 
 # Audio
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_PACKAGES += \

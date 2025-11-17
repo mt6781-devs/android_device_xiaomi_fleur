782c4f44fdcd43a5f3bd6279c9f6110d4f87a446
claxten10 <claxten10@gmail.com>
2025-11-17 00:16:35 +0000

    fleur: Build GS101 MemTrack
    Change-Id: I695878e2437ef6a5e1431ad7717a209133f9ddb3
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -108,7 +108,8 @@ PRODUCT_PACKAGES += \
 
 # Graphics
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.3-service
+    android.hardware.graphics.composer@2.3-service \
+    android.hardware.memtrack-service.mediatek-mali
 
 # Health
 PRODUCT_PACKAGES += \

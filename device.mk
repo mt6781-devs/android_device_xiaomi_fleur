52e481b67de69c200751cd69801a930505150239
claxten10 <claxten10@gmail.com>
2025-11-20 22:54:35 +0000

    fleur: Switch to dummy libaedv
    Change-Id: I0f87e8536103cd59b41a2d8ee322c861a352e31d
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -46,6 +46,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/google/interfaces \
     hardware/mediatek \
+    hardware/mediatek/libaedv \
     hardware/mediatek/libmtkperf_client \
     hardware/xiaomi
 

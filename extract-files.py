1b8fc95f99cdbddf0c125fd588af5620c484c638
claxten10 <claxten10@gmail.com>
2025-11-22 21:06:24 +0000

    fleur: Drop MTK Picture Quality stack
    * PQ does a lot of sharpenning, more than anyone would like, it rarely makes the
      picture actually look better.
    
    * We don't really have a custom PQ config for our panels anyway, therefore drop
      it.
    
    test:
     - open youtube
     - play video
     - looks normal, not oversharpenned
    Change-Id: I39281b4878ebd2272cd835d8124d0707d68c7390
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -36,7 +36,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
     ('vendor/lib/libnvram.so', 'vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
         .add_needed('libbase_shim.so'),
-    ('vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so', 'vendor/lib64/libmtkcam_stdutils.so', 'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.15-impl.so'): blob_fixup()
+    ('vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so', 'vendor/lib64/libmtkcam_stdutils.so'): blob_fixup()
         .replace_needed('libutils.so', 'libutils-v32.so'),
     'vendor/bin/hw/mtkfusionrild': blob_fixup()
         .add_needed('libutils-v32.so'),

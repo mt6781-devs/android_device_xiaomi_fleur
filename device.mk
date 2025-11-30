512341db4fa61050afe6d0ea5645ca9552509b95
claxten10 <claxten10@gmail.com>
2025-11-30 00:29:41 +0000

    fleur: Switch to source-built C2 service
    Change-Id: Ic3aa378363c2a7af1e7ea049467f10da196964f3
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -213,6 +213,11 @@ PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
 # Media
+$(call soong_config_set_bool,android_hardware_mediatek_codec2,link_v33_libstagefright_foundation,true)
+
+PRODUCT_PACKAGES += \
+    android.hardware.media.c2-mtk-service
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 

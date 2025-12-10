3f544e179590803c7a06eefb86e6c472b953690e
pjgowtham <pjgowtham@gmail.com>
2025-12-10 07:09:21 +0000

    fleur: Build dynamic sensors hal
    https://android.googlesource.com/platform/hardware/libhardware/+/refs/heads/main/modules/sensors/dynamic_sensor/README.md
    
    Change-Id: I2eaa47fb0b5f0c85241c079dfb85dd8a1d72e93d

--- device.mk
+++ device.mk
@@ -254,7 +254,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
-    android.hardware.sensors@2.0-subhal-impl-1.0:64
+    android.hardware.sensors@2.0-subhal-impl-1.0:64 \
+    sensors.dynamic_sensor_hal:64
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_ODM)/etc/sensors/hals.conf \
